e066cab5691852e1adc7af9b72e65f8bbb40e540
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-10-14 22:24:55 +0300

    ADD: HorizontalLine, U-styles

--- src/pages/SearchHistoryPage/SearchHistoryPage.jsx
+++ src/pages/SearchHistoryPage/SearchHistoryPage.jsx
@@ -3,16 +3,18 @@ import { DateFilter } from '../../components/DateFilter/DateFilter';
 import { Button } from '../../components/Button/Button';
 import '../../components/Section/Section.style.scss';
 import './SearchHistoryPage.style.scss';
+import '../WelcomePage/WelcomePage.style.scss';
 
 export const SearchHistoryPage = () => {
 	return (
 		<section className="section" aria-label="search history section">
-			<div className="container">
-				<div className="hero-title__container">
+			<div className="container no-flex">
+				<div className="hero-title__container push-content">
 					<Title text="History" />
 					<DateFilter />
 					<Button text="Clear all history" />
 				</div>
+				<div className="horizontal-line push-content" />
 			</div>
 		</section>
 	);

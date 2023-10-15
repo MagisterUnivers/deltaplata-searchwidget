3deea19d20a7857d8677c41e97a21ad171943eab
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-10-15 16:28:37 +0300

    ADD: Buttons to switch layers

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -8,6 +8,8 @@ import {
 import { Bulk } from '../../components/Bulk/Bulk';
 import PersonCard from '../../components/PersonCard/PersonCard';
 import { HashViewComponent } from '../../components/HashViewComponent/HashViewComponent';
+import { ReactComponent as GridSVG } from '../../assets/svg/grid-layer.svg';
+import { ReactComponent as FlexSVG } from '../../assets/svg/flex-layer.svg';
 import '../../components/Container/Container.style.scss';
 import '../WelcomePage/WelcomePage.style.scss';
 import '../../components/Section/Section.style.scss';
@@ -28,6 +30,22 @@ const SearchPage = () => {
 							Results for:
 							<span className={searchQueryClass}>{searchquery}</span>
 						</h1>
+					</div>
+					<div className="title-wrapper__grid-selection">
+						<FlexSVG
+							width={16}
+							height={16}
+							aria-label="switch layer to flex"
+							title="Switch to Flex View"
+							className="title-wrapper__layer-switch"
+						/>
+						<GridSVG
+							width={16}
+							height={16}
+							aria-label="switch later to grid"
+							title="Switch to Grid View"
+							className="title-wrapper__layer-switch"
+						/>
 					</div>
 					<div className="title-wrapper no-wrap place-to__end">
 						<Filter type="filter" data={MockFilterOptions} />

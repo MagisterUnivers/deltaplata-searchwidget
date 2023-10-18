49b27f1e9183fad7794e2ed3184fb4767b5bb128
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-10-18 18:55:17 +0300

    FIX: Loader Drag/Click

--- src/components/BulkUploader/BulkUploader.jsx
+++ src/components/BulkUploader/BulkUploader.jsx
@@ -10,7 +10,7 @@ export function BulkUploader () {
 
   function handleImageUpload (e) {
     const file = e.target.files[0]
-    const reader = new FileReader()
+    const reader = new FileReader() //eslint-disable-line
 
     reader.onloadend = () => {
       setFormData((prevFormData) => ({
@@ -24,14 +24,42 @@ export function BulkUploader () {
     }
   }
 
+  function handleDragOver (e) {
+    e.preventDefault()
+  }
+
+  function handleDrop (e) {
+    e.preventDefault()
+    const file = e.dataTransfer.files[0]
+
+    if (file) {
+      const reader = new FileReader() //eslint-disable-line
+
+      reader.onloadend = () => {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          userImgUrl: reader.result
+        }))
+      }
+
+      reader.readAsDataURL(file)
+    }
+  }
+
   function handleAvatarClick () {
     if (fileInputRef.current) {
       fileInputRef.current.click()
     }
   }
 
+  console.log(formData)
+
   return (
-    <div className='bulk-loader'>
+    <div
+      className='bulk-loader' onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      style={formData.userImgUrl ? { backgroundImage: `url(${formData.userImgUrl})` } : {}}
+    >
       <div className='bulk-loader__content-wrapper'>
         <UploadSVG width={125} height={125} className='bulk-loader__icon' onClick={handleAvatarClick} />
         <p className='bulk-loader__subtext'>Max 20MB (CSV/XLSX)</p>
